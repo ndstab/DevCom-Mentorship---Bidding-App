c6eaa0095a4cf94f54c186739f540881d25c3287
Surhud Naik <naiksurhud@gmail.com>
2025-01-20 19:36:47 +0530

    removed my-app

---
+++ add-product/src/App.js
@@ -0,0 +1,11 @@
+
+import Information from "./Information";
+function App() {
+  return (
+    <div className="App">
+      <Information prompt = "Hello" />
+    </div>
+  );
+}
+
+export default App;
---
+++ add-product/src/Information.js
@@ -0,0 +1,10 @@
+function Information(props) {
+    return (
+        <>
+            <h1> {props.prompt} </h1>
+            <input type = "text" ></input>
+        </>
+    )
+}
+
+export default Information;
