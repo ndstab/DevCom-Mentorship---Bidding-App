10fe98113d0267309973c71f58715d8b591538b9
icodeforlife24 <shiviutkarsh@gmail.com>
2025-01-28 14:28:55 +0530

    authentication

--- myproject/bidding/views.py
+++ myproject/bidding/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import  login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
